ef4c8b838bfc9d8286edb6f8903de41faf655f69
nguyenngocthanh-collab <nguyenngocthanh2ndmail@gmail.com>
2025-10-29 22:02:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('quizForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const result = document.getElementById('result');
-  result.innerText = 'Cảm ơn bạn đã tham gia! Kết quả sẽ được công bố sau!';
-  this.reset();
-});
