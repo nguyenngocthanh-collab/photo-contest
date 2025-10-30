1e2f5a6c0b7d5a9b604d3d4a4c55b18c6138a3e8
nguyenngocthanh-collab <nguyenngocthanh2ndmail@gmail.com>
2025-10-30 13:40:53 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const data = req.body;
 
     // gửi request lên Google Apps Script
-    const response = await fetch("YOUR_APPS_SCRIPT_WEBHOOK_URL", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwpYZX3eghohJ67EnbqAtdfqfaPbMnQf7nSVNqgknr6rjR83HiL3REkUkblxRDkKBTs/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
