f6c2b63982f4fb785009f24e62c536d0b2b98cb4
nguyenngocthanh-collab <nguyenngocthanh2ndmail@gmail.com>
2025-11-02 21:10:57 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
-  const APPS_SCRIPT_WEBHOOK = "https://script.google.com/macros/s/AKfycbzlboDrk1VT6Rg_IoY8TWe_9UHdQR0NZs2tKyjp8ID21U8VdqBheCNx-jej9qWA5ThV/exec";
+  const APPS_SCRIPT_WEBHOOK = "https://script.google.com/macros/s/AKfycbyPyt_4vqwRGdBrXkhWzY8a34-gxwtz4hbzWK5l0m6VqYICk5_2JkTD0q9tkuFbnO2Z/exec";
 
   try {
     const response = await fetch(APPS_SCRIPT_WEBHOOK, {
