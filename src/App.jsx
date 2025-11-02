08f77dadd8ac7cb7ce0c69fd23450ac194e9026d
nguyenngocthanh-collab <nguyenngocthanh2ndmail@gmail.com>
2025-11-02 13:26:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const ENTRY_CLASS = "entry.2121292925";
 const ENTRY_DEPT = "entry.544016220";
 
 // Apps Script webhook (sau khi deploy) - nếu để "" thì ảnh không được lưu tự động.
-const APPS_SCRIPT_WEBHOOK = "https://script.google.com/macros/s/AKfycbzjYNCmkYVbFKvunF4xSXb51BxLq2TWubZGzcySq2vlM2x4wCTByrvjT1WW0h4rEsCP/exec"; // ví dụ: "https://script.google.com/macros/s/AKfycbxxx/exec"
+const APPS_SCRIPT_WEBHOOK = "https://script.google.com/macros/s/AKfycbyTb0RLU68E0HqYAOm5WngVXth-GGgooygj_gud0jPngtl8Y_KD6eTCiJuH9cQL-39x/exec"; // ví dụ: "https://script.google.com/macros/s/AKfycbxxx/exec"
 
 // --- END CONFIG ---
 export default function App() {
