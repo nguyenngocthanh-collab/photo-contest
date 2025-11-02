548a85fd7b6586150e8cb8c24247a458c11e15fc
nguyenngocthanh-collab <nguyenngocthanh2ndmail@gmail.com>
2025-11-02 22:42:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./styles.css"; // <--- sửa từ style.css hoặc /src/styles.css
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
